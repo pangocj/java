3f927f2a1e9522e6397b7009bac2371a25a560be
itwill <itwill@DESKTOP-KHEFSBN>
2023-03-29 15:55:49 +0900

    2Day

---
+++ java_1/src/basic/DataTypeApp.java
@@ -0,0 +1,31 @@
+package basic;
+
+//자료형(DataType) : 값을 표현하기 위한 키워드 또는 특정 대상을 표현하기 위한 식별자
+// => 값이나 특정 대상을 저장하기 위한 변수를 선언하기 위해 사용
+//1.원시형(PrimitiveType) : 값을 표현하기 위한 자료형 - 키워드
+// => 숫자형(정수형, 실수형, 문자형) - 연산 가능 , 논리형
+//2.참조형(ReferenceType) : 특정 대상을 표현하기 위한 자료형 - 식별자(클래스)
+// => String 클래스 : 문자열을 표현하기 위한 자료형(클래스)
+
+public class DataTypeApp {
+	public static void main(String[] args) {
+		//정수형(IntegerType) 리터럴 : 소숫점이 없는 숫자값
+		// => Java에서 정수형 리터럴은 기본적으로 4Byte(Integer)로 표현 >> -2147483648~2147483647
+		System.out.println("<<정수형(IntegerType)>>");
+		System.out.println("정수형(10진수 100) = "+100);
+	}
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
