76e835fa5bad494b559ebcbae566c3481f33f5d1
itwill <itwill@DESKTOP-KHEFSBN>
2023-03-31 16:28:47 +0900

    4Day

--- java_1/src/basic/SwitchApp.java
+++ java_1/src/basic/SwitchApp.java
@@ -72,29 +72,35 @@ public class SwitchApp {
 		
 		//변수값이 0~100 범위의 유효값인지 아닌지를 구분하여 출력하는 프로그램 작성
 		if(jumsu >= 0 && jumsu <= 100) {
+			//변수값으로 등급을 구분하여 출력하는 프로그램 작성
+			//100 ~ 90 : A, 89 ~ 80 : B, 79 ~ 70 : C, 69 ~ 60 : D, 59 ~ 0 : F
+			String grade;//학점을 저장하기 위한 변수
 			
+			switch(jumsu/10) {
+			case 10:
+			case 9: grade="A"; break;
+			case 8: grade="B"; break;
+			case 7: grade="C"; break;
+			case 6: grade="D"; break;
+			default: grade="F";
+			}
+			
+			System.out.println("[결과]"+jumsu+"점 = "+grade+"학점");
 		} else {
-			
+			System.out.println("[에러]0~100 범위를 벗어난 비정상적인 점수가 입력 되었습니다.");
+		}
+		System.out.println("==============================================================");
+		String kor="둘";
+		String eng="";
+		
+		//문자열을 비교하여 명령 선택 실행 가능
+		switch(kor) {
+		case "하나": eng="One"; break;
+		case "둘": eng="Two"; break;
+		case "셋": eng="Three"; break;
 		}
 		
-		
+		System.out.println("[결과]"+kor+" = "+eng);
+		System.out.println("==============================================================");
 	}
 }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
