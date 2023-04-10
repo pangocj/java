4447a46ecda7f0e97581f3964f14703e40856465
itwill <itwill@DESKTOP-KHEFSBN>
2023-04-10 12:43:27 +0900

    10Day

--- java_2/src/association/Car.java
+++ java_2/src/association/Car.java
@@ -23,8 +23,50 @@ public class Car {
 	private String modelName;
 	private int productionYear;
 	//엔진정보를 저장하기 위한 필드 - Engine 클래스를 자료형으로 선언된 필드
-	// => 필드에는 Engine 객체를 제공받아 저장 - 포함관계
+	// => 필드에는 생성자 또는 Setter 메소드를 사용하여 Engine 객체를 제공받아 저장 - 포함관계
+	private Engine carEngine;
 	
+	public Car() {
+		// TODO Auto-generated constructor stub
+	}
+
+	public Car(String modelName, int productionYear, Engine carEngine) {
+		super();
+		this.modelName = modelName;
+		this.productionYear = productionYear;
+		this.carEngine = carEngine;
+	}
+
+	public String getModelName() {
+		return modelName;
+	}
+
+	public void setModelName(String modelName) {
+		this.modelName = modelName;
+	}
+
+	public int getProductionYear() {
+		return productionYear;
+	}
+
+	public void setProductionYear(int productionYear) {
+		this.productionYear = productionYear;
+	}
+
+	public Engine getCarEngine() {
+		return carEngine;
+	}
+
+	public void setCarEngine(Engine carEngine) {
+		this.carEngine = carEngine;
+	}
+	
+	//자동차 정보(필드값)를 출력하는 메소드
+	public void displayCar() {
+		System.out.println("모델명 = "+modelName);
+		System.out.println("생산년도 = "+productionYear);
+		System.out.println("엔진정보 = "+carEngine);
+	}
 }
 
 
