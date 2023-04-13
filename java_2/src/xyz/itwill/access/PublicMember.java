1faf827c7b621fe716072fb762b4363bfe6718e5
itwill <itwill@DESKTOP-KHEFSBN>
2023-04-13 16:04:33 +0900

    13Day

--- java_2/src/xyz/itwill/access/PublicMember.java
+++ java_2/src/xyz/itwill/access/PublicMember.java
@@ -1,6 +1,6 @@
 package xyz.itwill.access;
 
-//public : 모든 패키지의 클래스에서 접근 가능하도록 설정
+//public 접근 제한자 : 모든 패키지의 클래스에서 접근 가능하도록 설정
 
 //public 접근 제한자를 사용하여 클래스 선언 - public 클래스
 // => public 클래스로 설정된 클래스는 모든 패키지의 클래스에서 접근 가능
