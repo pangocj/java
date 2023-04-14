8c29260bd97e1ae7627d7a11b8742da30bc4e20d
itwill <itwill@DESKTOP-KHEFSBN>
2023-04-14 13:19:26 +0900

    14Day

---
+++ java_3/src/xyz/itwill/lang/SystemApp.java
@@ -0,0 +1,17 @@
+package xyz.itwill.lang;
+
+//Java API(Application Programming Interface) : Java 프로그램 개발에 프로그램 기능을 제공
+//하기 위한 도구
+// => 라이브러리를 이용하여 배포된 Java 자료형(클래스, 인터페이스, 열거형)
+
+//java.lang 패키지 : Java 프로그램 개발에 필요한 기본 자료형을 제공하는 패키지
+// => java.lang 패키지의 자료형은 import 처리를 하지 않아도 접근 가능
+
+//Object 클래스 : 모든 Java 클래스가 반드시 상속받는 부모클래스
+// => 모든 Java 클래스를 대신하여 사용할 수 있는 대표클래스
+
+
+
+public class SystemApp {
+
+}
