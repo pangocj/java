e27b54d7031b1e81bef055c957a0ff22acc4db25
itwill <itwill@DESKTOP-KHEFSBN>
2023-04-17 11:08:49 +0900

    14Day

--- java_3/src/xyz/itwill/lang/SystemApp.java
+++ java_3/src/xyz/itwill/lang/SystemApp.java
@@ -2,8 +2,7 @@ package xyz.itwill.lang;
 
 import java.util.Scanner;
 
-//Java API(Application Programming Interface) : Java 프로그램 개발에 프로그램 기능을 제공
-//하기 위한 도구
+//Java API(Application Programming Interface) : Java 프로그램 개발에 필요한 기능을 제공하기 위한 도구
 // => 라이브러리를 이용하여 배포된 Java 자료형(클래스, 인터페이스, 열거형)
 // => API 문서를 활용하여 Java 자료형에 대한 설명을 제공받아 사용
 // => 온라인 API 문서 : https://docs.oracle.com/en/java/javase/17/docs/api/index.html
@@ -13,8 +12,9 @@ import java.util.Scanner;
 
 //Object 클래스 : 모든 Java 클래스가 반드시 상속받는 부모클래스
 // => 모든 Java 클래스를 대신하여 사용할 수 있는 대표클래스
-//Math 클래스 : 수학 관련 기능을 메소드로 제공하는 클래스
-//Class 클래스 : 메모리(MethodArea)에 저장된 클래스를 관련 정보를 저장하기 위한 클래스
+//Class 클래스 : 메모리(MethodArea)에 저장된 클래스(Class 객체 - Clazz) 관련 기능을 메소드로
+//제공하기 위한 클래스
+//Math 클래스 : 수학 관련 기능을 메소드로 제공하기 위한 클래스
 
 //System 클래스 : 표준 입출력 스트림을 제공하거나 시스템 관련 기능을 메소드로 제공하는 클래스
 public class SystemApp {
@@ -28,9 +28,12 @@ public class SystemApp {
 		
 		if(num == 0) {
 			System.out.println("[메세지]프로그램을 강제로 종료합니다.");
-			//System.exit(int status) : 프로그램을 강제로 강제로 종료하는 메소드
+			//System.exit(int status) : 프로그램을 강제로 종료하는 메소드
 			System.exit(0);
 		}
+		
+		//System.gc() : 메모리를 청소하는 프로그램(Garbage Collector)을 실행하는 메소드
+		System.gc();
 		
 		//System.currentTimeMillis() : 시스템의 현재 날짜와 시간에 대한 타임스템프를 반환하는 메소드
 		//타임스템프(TimeStamp) : 날짜와 시간을 정수값으로 표현하기 위해 만들어진 시간값
@@ -45,9 +48,6 @@ public class SystemApp {
 		long endTime=System.currentTimeMillis();
 		
 		System.out.println("실행시간 = "+(endTime-startTime)+"ms");
-		
-		//System.gc() : 메모리를 청소하는 프로그램(Garbage Collector)을 실행하는 메소드
-		System.gc();
 		
 		scanner.close();
 	}
