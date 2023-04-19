d9dc5b4ed63ce4bb0ded4f53ec85deab6c39c098
itwill <itwill@DESKTOP-KHEFSBN>
2023-04-19 12:12:30 +0900

    16Day

--- java_3/src/xyz/itwill/lang/ConsoleCalculateApp.java
+++ java_3/src/xyz/itwill/lang/ConsoleCalculateApp.java
@@ -35,7 +35,7 @@ public class ConsoleCalculateApp {
 		}
 		
 		//연산자가 없거나 연산자가 잘못된 위치에 있는 경우
-		if(index <= 0 || index > operation.length()-1) {
+		if(index <= 0 || index >= operation.length()-1) {
 			System.out.println("[에러]연산식을 잘못 입력 하였습니다.");
 			System.exit(0);
 		}
