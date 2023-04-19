41f790efafccb3ee81d3d31941fef00a1a1d7a2e
itwill <itwill@DESKTOP-KHEFSBN>
2023-04-19 14:52:32 +0900

    16Day

--- java_3/src/xyz/itwill/util/BaseballGameApp.java
+++ java_3/src/xyz/itwill/util/BaseballGameApp.java
@@ -17,9 +17,6 @@ public class BaseballGameApp {
 		// 0~9 범위의 정수값 3개를 저장하기 위한 배열 선언 - 난수값을 저장하기 위한 배열
 		int[] dap=new int[3];
 		
-		// 0~9 범위의 정수값 3개를 저장하기 위한 배열 선언 - 입력값을 저장하기 위한 배열
-		int[] num=new int[3];
-		
 		//규칙에 맞는 난수값을 배열 요소에 저장되도록 처리하기 위한 반복문
 		while(true) {
 			for(int i=0;i<dap.length;i++) {
@@ -30,6 +27,13 @@ public class BaseballGameApp {
 		}
 		
 		Scanner scanner=new Scanner(System.in);
+
+		// 0~9 범위의 정수값 3개를 저장하기 위한 배열 선언 - 입력값을 저장하기 위한 배열
+		int[] num=new int[3];
+		
+		//정답 관련 상태정보를 저장하기 위한 변수 선언
+		// => false : 정답을 못맞춘 상태, true : 정답을 맞춘 상태
+		boolean result=false;
 		
 		//키보드로 정수값을 입력받아 비교 처리하는 기회를 제공하기 위한 반복문
 		for(int cnt=1;cnt<=15;cnt++) {
@@ -62,19 +66,39 @@ public class BaseballGameApp {
 				if(num[0]!=0 && num[0]!=num[1] && num[1]!=num[2] && num[2]!=num[0]) break;
 				System.out.println("[에러]0으로 시작되거나 중복된 숫자가 존재합니다.");
 			}
+			
+			//난수값과 입력값을 비교하여 스트라이크와 볼을 구분하여 계산
+			// => 난수값이 저장된 배열의 요소값과 입력값이 저장된 배열의 요소값을 교차 비교
+			for(int i=0;i<dap.length;i++) {//난수값이 저장된 배열 요소를 처리하기 위한 반복문
+				for(int j=0;j<num.length;j++) {//입력값이 저장된 배열 요소를 처리하기 위한 반복문
+					if(dap[i] == num[j]) {//요소값이 같은 경우
+						if(i == j) {//요소값이 저장된 위치(Index)가 같은 경우
+							strike++;//스트라이크 증가
+						} else {
+							ball++;//볼 증가
+						}
+					}
+				}
+			}
+			
+			//처리 결과 출력
+			if(strike == 3) {
+				System.out.println("[메세지]축하합니다. "+cnt+"번만에 맞췄습니다.");
+				result=true;
+				break;
+			}
+			
+			System.out.println("[결과]"+strike+"스트라이크 "+ball+"볼");
 		}
+		
+		if(!result) {//정답을 맞추지 못한 경우
+			System.out.print("[메세지]정답을 못맞췄군요. 정답은 [");
+			for(int su : dap) {
+				System.out.print(su);
+			}
+			System.out.println("]입니다.");
+		}
+		
+		scanner.close();
 	}
 }
-
-
-
-
-
-
-
-
-
-
-
-
-
