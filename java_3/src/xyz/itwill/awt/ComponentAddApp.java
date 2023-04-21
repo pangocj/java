2171c214923934ff28f3d3bd84cfccaada30a1b1
itwill <itwill@DESKTOP-KHEFSBN>
2023-04-21 15:24:09 +0900

    18Day

--- java_3/src/xyz/itwill/awt/ComponentAddApp.java
+++ java_3/src/xyz/itwill/awt/ComponentAddApp.java
@@ -11,7 +11,7 @@ public class ComponentAddApp extends Frame {
 		
 		//Button 클래스 : 버튼 컴퍼넌트을 구현하기 위한 클래스
 		// => Button 클래스의 Button(String label) 생성자로 버튼의 이름이 설정된 Button 객체 생성
-		Button button=new Button("Button");
+		Button button=new Button("BUTTON");
 		
 		//Container.add(Component c) : 컨테이너에 컴퍼넌트를 배치하는 메소드
 		// => 컨테이너에 설정된 배치관리자(LayoutManager)에 의해 컴퍼넌트가 출력 위치와 
