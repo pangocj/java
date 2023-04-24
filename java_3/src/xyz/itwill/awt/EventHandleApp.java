fab3f8caaffcad4d8ecdc4fa143017d8760adf81
itwill <itwill@DESKTOP-KHEFSBN>
2023-04-24 11:07:28 +0900

    18Day

--- java_3/src/xyz/itwill/awt/EventHandleApp.java
+++ java_3/src/xyz/itwill/awt/EventHandleApp.java
@@ -17,7 +17,7 @@ import java.awt.event.ActionListener;
 // => 이벤트와 관련된 XXXEvent 클래스(이벤트 정보를 저장하기 위한 클래스)로 객체가 자동 생성
 // ex) Button 컴퍼넌트를 누른 경우 ActionEvent 클래스로 객체 생성 - ActionEvent 발생
 //2.이벤트 소스에서 발생된 이벤트를 처리하기 위한 클래스 작성
-// => 이벤트를 처리하기 위한 XXXListener 인터페이스를 상속받아 이벤트 처리 클래스 작성
+// => 이벤트를 처리하기 위해 XXXListener 인터페이스를 상속받아 이벤트 처리 클래스 작성
 // ex) ActionEvent >> ActionListener 인터페이스를 상속받아 이벤트 처리 클래스 작성
 // => Listener 인터페이스에서 컴퍼넌트에서 발생된 이벤트를 처리하기 위해 추상메소드 제공
 // => Listener 인터페이스를 상속받은 자식클래스에서 추상메소드를 오버라이드 선언하여
@@ -25,8 +25,9 @@ import java.awt.event.ActionListener;
 //3.이벤트 소스에서 이벤트가 발생되면 이벤트 처리 클래스의 객체를 제공받아 이벤트 처리 명령이
 //실행되도록 설정 - Component.addXXXListener(Listener l) 메소드 호출
 // => Component.addXXXListener(Listener l) : 컴퍼넌트의 이벤트를 처리하기 위한 이벤트 처리 
-//객체를 등록하기 위한 메소드
-// => 이벤트 처리 객체를 제공받아 자동으로 이벤트 처리 메소드를 호출하여 이벤트 처리
+//객체를 매개변수로 제공받아 등록(추가)하기 위한 메소드
+// => JVM은 컴퍼넌트에서 이벤트가 발생되면 등록된 이벤트 처리 객체를 사용하여 자동으로 
+//이벤트 처리 메소드를 호출하여 이벤트 처리
 
 //[EXIT] 버튼을 누른 경우 프로그램을 종료하는 기능의 프로그램 작성
 public class EventHandleApp extends Frame {
@@ -57,7 +58,7 @@ public class EventHandleApp extends Frame {
 //이벤트 처리 기능을 제공하기 위한 클래스 - Listener 인터페이스를 상속받아 작성
 // => Button 컴퍼넌트에서 발생된 ActionEvent를 처리하기 위한 클래스
 class EventHandle implements ActionListener {
-	//이벤트 처리 명령을 작성하기 위한 메소드
+	//이벤트 처리 명령을 작성하기 위한 메소드 - 이벤트 처리 메소드
 	@Override
 	public void actionPerformed(ActionEvent e) {
 		System.exit(0);
