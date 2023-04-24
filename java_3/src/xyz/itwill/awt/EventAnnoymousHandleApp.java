9859c29a176a6b6ca66edda664d03317a4269868
itwill <itwill@DESKTOP-KHEFSBN>
2023-04-24 11:38:36 +0900

    19Day

--- java_3/src/xyz/itwill/awt/EventAnnoymousHandleApp.java
+++ java_3/src/xyz/itwill/awt/EventAnnoymousHandleApp.java
@@ -4,9 +4,13 @@ import java.awt.Button;
 import java.awt.FlowLayout;
 import java.awt.Font;
 import java.awt.Frame;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
+//[EXIT] 버튼을 누른 경우 프로그램을 종료하는 기능의 프로그램 작성
+// => 이벤트 처리 클래스를 익명의 내부클래스(Anonymous InnerClass)로 작성
 public class EventAnnoymousHandleApp extends Frame {
-private static final long serialVersionUID = 1L;
+	private static final long serialVersionUID = 1L;
 	
 	public EventAnnoymousHandleApp(String title) {
 		super(title);
@@ -15,6 +19,14 @@ private static final long serialVersionUID = 1L;
 		Button exit=new Button("EXIT");//이벤트 소스
 		exit.setFont(new Font(Font.SANS_SERIF, Font.BOLD, 30));
 		add(exit);
+
+		//이벤트 처리 객체를 익명의 내부클래스를 사용하여 생성
+		exit.addActionListener(new ActionListener() {
+			@Override
+			public void actionPerformed(ActionEvent e) {
+				System.exit(0);	
+			}
+		});
 		
 		setBounds(800, 200, 300, 300);
 		setVisible(true);
@@ -24,3 +36,8 @@ private static final long serialVersionUID = 1L;
 		new EventAnnoymousHandleApp("이벤트처리");
 	}
 }
+
+
+
+
+
