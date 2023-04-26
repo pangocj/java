efcdb439fa31b47604556fd7f62f72b9dad1fb63
itwill <itwill@DESKTOP-KHEFSBN>
2023-04-26 16:15:08 +0900

    21Day

--- java_3/src/xyz/itwill/io/NotepadApp.java
+++ java_3/src/xyz/itwill/io/NotepadApp.java
@@ -1,7 +1,6 @@
 package xyz.itwill.io;
 
 import java.awt.BorderLayout;
-import java.awt.FileDialog;
 import java.awt.Font;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
@@ -9,11 +8,13 @@ import java.awt.event.InputEvent;
 import java.awt.event.KeyEvent;
 import java.io.BufferedReader;
 import java.io.BufferedWriter;
+import java.io.File;
 import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.io.FileWriter;
 import java.io.IOException;
 
+import javax.swing.JFileChooser;
 import javax.swing.JFrame;
 import javax.swing.JMenu;
 import javax.swing.JMenuBar;
@@ -22,6 +23,7 @@ import javax.swing.JOptionPane;
 import javax.swing.JScrollPane;
 import javax.swing.JTextArea;
 import javax.swing.KeyStroke;
+import javax.swing.filechooser.FileNameExtensionFilter;
 
 //문서파일 편집기 프로그램 작성 - 메모장
 public class NotepadApp extends JFrame {
@@ -30,11 +32,11 @@ public class NotepadApp extends JFrame {
 	JTextArea jTextArea;
 	JMenuItem init, open, save, exit;
 	
-	//FileDialog 클래스 : 파일을 선택하기 위한 다이얼로그를 생성하기 위한 컴퍼넌트 클래스
-	FileDialog openDialog, saveDialog;
+	//JFileChooser 클래스 : 파일을 선택하기 위한 다이얼로그를 생성하기 위한 컴퍼넌트 클래스
+	JFileChooser openDialog, saveDialog;
 	
 	//현재 작업중인 문서파일의 경로를 저장하기 위한 필드
-	private String filepath;
+	private File file;
 	
 	public NotepadApp(String title) {
 		super(title);
@@ -72,8 +74,15 @@ public class NotepadApp extends JFrame {
 		
 		getContentPane().add(jScrollPane, BorderLayout.CENTER);
 		
-		openDialog=new FileDialog(this, "열기", FileDialog.LOAD);
-		saveDialog=new FileDialog(this, "저장", FileDialog.SAVE);
+		openDialog=new JFileChooser();
+		//JFileChooser.setCurrentDirectory(File file) : 파일 다이얼로그의 기본 작업 
+		//디렉토리를 변경하는 메소드 
+		openDialog.setCurrentDirectory(new File("c:/"));
+		//JFileChooser.addChoosableFileFilter(FileFilter filter) : 파일 선택에 대한 필터 기능을 추가하는 메소드 
+		openDialog.addChoosableFileFilter(new FileNameExtensionFilter("텍스트 파일(*.txt)", "txt"));
+		saveDialog=new JFileChooser();
+		saveDialog.setCurrentDirectory(new File("c:/"));
+		saveDialog.addChoosableFileFilter(new FileNameExtensionFilter("텍스트 파일(*.txt)", "txt"));
 
 		init.addActionListener(new NodepadEventHandle());
 		open.addActionListener(new NodepadEventHandle());
@@ -96,66 +105,69 @@ public class NotepadApp extends JFrame {
 			
 			if(eventSource==init) {
 				jTextArea.setText("");//JTextArea 컴퍼넌트 초기화
-				filepath="";//필드 초기화
+				file=null;//필드 초기화
 				setTitle("제목 없음 - Java 메모장");//프레임 제목 초기화
 			} else if(eventSource==open) {
-				//열기 관련 파일 다이얼로그를 화면에 출력
-				// => 파일을 선택하거나 선택을 취소한 경우 파일 다이얼로그는 자동으로 숨김 처리
-				openDialog.setVisible(true);
+				//JFileChooser.showOpenDialog(Component parent) : 열기 관련 파일 다이얼로그를
+				//화면에 출력하는 메소드 - 버튼 선택에 따른 정수값 반환
+				int option=openDialog.showOpenDialog(NotepadApp.this);
 				
-				//FileDialog.getFile() : 선택된 파일의 이름을 반환하는 메소드
-				if(openDialog.getFile()==null) return;//파일 선택을 취소한 경우 메소드 종료
-				
-				//FileDialog.getDirectory() : 선택된 파일의 디렉토리 경로를 반환하는 메소드
-				filepath=openDialog.getDirectory()+openDialog.getFile();//선택된 파일의 경로 저장
-
-				setTitle(openDialog.getFile()+" - Java 메모장");
-
-				try {
-					//파일 경로를 제공받아 파일 입력스트림 생성
-					BufferedReader in=new BufferedReader(new FileReader(filepath));
+				if(option==JFileChooser.APPROVE_OPTION) {//파일 선택 버튼을 누른 경우
+					//JFileChooser.getSelectedFile() : 선택된 파일의 경로가 저장된 File 
+					//객체를 반환하는 메소드
+					file=openDialog.getSelectedFile();
 					
-					jTextArea.setText("");//JTextArea 컴퍼넌트 초기화
+					setTitle(file.toString()+" - Java 메모장");
 					
-					while(true) {
-						//파일에 저장된 값을 한줄씩 읽어 문자열로 반환받아 저장
-						String text=in.readLine();
-						if(text==null) break;//파일내용이 없는 경우 반복문 종료
-						//변수에 저장된 문자열을 JTextArea 컴퍼넌트에 추가하여 출력
-						jTextArea.append(text+"\n");
+					try {
+						//파일의 절대경로를 제공받아 파일 입력스트림 생성
+						BufferedReader in=new BufferedReader(new FileReader(file.getAbsoluteFile()));
+						
+						jTextArea.setText("");//JTextArea 컴퍼넌트 초기화
+						
+						while(true) {
+							//파일에 저장된 값을 한줄씩 읽어 문자열로 반환받아 저장
+							String text=in.readLine();
+							if(text==null) break;//파일내용이 없는 경우 반복문 종료
+							//변수에 저장된 문자열을 JTextArea 컴퍼넌트에 추가하여 출력
+							jTextArea.append(text+"\n");
+						}
+						
+						in.close();
+					} catch (FileNotFoundException exception) {
+						JOptionPane.showMessageDialog(null, "파일을 찾을 수 없습니다.");
+					} catch (IOException exception) {
+						JOptionPane.showMessageDialog(null, "프로그램에 문제가 발생 하였습니다.");
 					}
-					
-					in.close();
-				} catch (FileNotFoundException exception) {
-					JOptionPane.showMessageDialog(null, "파일을 찾을 수 없습니다.");
-				} catch (IOException exception) {
-					JOptionPane.showMessageDialog(null, "프로그램에 문제가 발생 하였습니다.");
+				} else if(option==JFileChooser.CANCEL_OPTION) {//취소 버튼을 누른 경우
+					return;
 				}
 			} else if(eventSource==save) {
-				if(filepath==null || filepath.equals("")) {
-					//저장 관련 파일 다이얼로그를 화면에 출력
-					saveDialog.setVisible(true);
+				if(file==null) {
+					int option=saveDialog.showSaveDialog(NotepadApp.this);
 					
-					if(saveDialog.getFile()==null) return;//파일 선택을 취소한 경우 메소드 종료
-					
-					filepath=saveDialog.getDirectory()+saveDialog.getFile();//선택된 파일의 경로 저장
-					
-					setTitle(saveDialog.getFile()+" - Java 메모장");
-				}
-
-				try {
-					//파일 경로를 제공받아 파일 출력스트림 생성
-					BufferedWriter out=new BufferedWriter(new FileWriter(filepath));
-					
-					//JTextArea 컴퍼넌트의 모든 문자열을 반환받아 저장
-					String text=jTextArea.getText();
-					
-					//반환받은 문자열을 파일 출력스트림으로 전달하여 저장
-					out.write(text);
-					
-					out.close();
-				} catch (IOException exception) {
-					JOptionPane.showMessageDialog(null, "프로그램에 문제가 발생 하였습니다.");
+					if(option==JFileChooser.APPROVE_OPTION) {
+						file=saveDialog.getSelectedFile();
+						
+						setTitle(file.toString()+" - Java 메모장");
+						
+						try {
+							//파일의 경로를 제공받아 파일 출력스트림 생성
+							BufferedWriter out=new BufferedWriter(new FileWriter(file.getAbsoluteFile()));
+							
+							//JTextArea 컴퍼넌트의 모든 문자열을 반환받아 저장
+							String text=jTextArea.getText();
+							
+							//반환받은 문자열을 파일 출력스트림으로 전달하여 저장
+							out.write(text);
+							
+							out.close();
+						} catch (IOException exception) {
+							JOptionPane.showMessageDialog(null, "프로그램에 문제가 발생 하였습니다.");
+						}
+					} else if(option==JFileChooser.CANCEL_OPTION) {
+						return;
+					}
 				}
 			} else if(eventSource==exit) {
 				System.exit(0);
