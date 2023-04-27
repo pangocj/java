eb7a097e1b8f65d0b7f51bc6388cb5623b26cec8
itwill <itwill@DESKTOP-KHEFSBN>
2023-04-27 12:42:42 +0900

    22Day

--- java_3/src/xyz/itwill/net/TimeServerApp.java
+++ java_3/src/xyz/itwill/net/TimeServerApp.java
@@ -1,8 +1,10 @@
 package xyz.itwill.net;
 
 import java.io.IOException;
+import java.io.ObjectOutputStream;
 import java.net.ServerSocket;
 import java.net.Socket;
+import java.util.Date;
 
 //접속된 클라이언트에게 서버 컴퓨터의 현재 날짜와 시간을 전달하는 서버 프로그램 작성
 // => NTP(Network Time Protocol) Server : 날짜와 시간을 제공하는 서버 컴퓨터
@@ -28,22 +30,34 @@ public class TimeServerApp {
 				//클라이언트의 소켓과 연결될 소켓을 생성하여 반환하고 스레드 재실행
 				Socket socket=ntpServer.accept();
 				
-				System.out.println("socket = "+socket);
+				//System.out.println("socket = "+socket);
+				
+				/**
+				//Socket.getOutputStream() : Socket 객체의 출력스트림(OutputStream 객체)을 
+				//반환하는 메소드 
+				OutputStream stream=socket.getOutputStream();
+
+				//OutputStream 객체를 전달받아 객체를 전달할 수 있는 출력스트림으로 확장
+				ObjectOutputStream out=new ObjectOutputStream(stream);
+				
+				//출력스트림을 이용하여 시스템의 현재 날짜와 시간이 저장된 Date 객체를 생성
+				//하여 전달 - 클라이언트에게 날짜와 시간을 전송
+				out.writeObject(new Date());
+				*/
+				
+				new ObjectOutputStream(socket.getOutputStream()).writeObject(new Date());
+				
+				//로그 처리 - 기록
+				//Socket.getInetAddress() : 소켓과 연결된 외부 컴퓨터의 네트워크 식별자가  
+				//저장된 InetAddress 객체를 반환하는 메소드
+				System.out.println("[정보]클라이언트["+socket.getInetAddress()
+					.getHostAddress()+"]에게 날짜와 시간을 제공 하였습니다.");
+				
+				//클라이언트와의 접속 해제
+				socket.close();
 			}
 		} catch (IOException e) {
-			// TODO: handle exception
+			System.out.println("[에러]서버 네트워크에 문제가 발생 되었습니다.");
 		}
 	}
 }
-
-
-
-
-
-
-
-
-
-
-
-
