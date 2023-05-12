512c3ed4e2538f305324992553c1dceb5cdabd27
user <user@BOOK-LCCUUB9T5H>
2023-05-12 12:24:13 +0900

    32Day

--- jdbc/src/xyz/itwill/jdbc/InsertStudentApp.java
+++ jdbc/src/xyz/itwill/jdbc/InsertStudentApp.java
@@ -1,8 +1,8 @@
 package xyz.itwill.jdbc;
 
+import java.sql.Connection;
 import java.sql.DriverManager;
-
-import oracle.jdbc.driver.OracleDriver;
+import java.sql.SQLException;
 
 //JDBC(Java DataBase Connectivity) : Java를 사용하여 DBMS 서버에 접속해 SQL 명령을 전달하여
 //실행하기 위한 기능을 제공하는 Java API(클래스 또는 인터페이스)
@@ -30,13 +30,46 @@ import oracle.jdbc.driver.OracleDriver;
 //STUDENT 테이블에 학생정보를 삽입하는 JDBC 프로그램 작성
 public class InsertStudentApp {
 	public static void main(String[] args) {
-		//1.OracleDriver 클래스로 객체를 생성하여 DriverManager 클래스의 JDBC Driver 객체로 등록
-		//JDBC Driver 객체 : DriverManager 클래스에 등록되어 관리되는 Driver 객체
-		//Driver 객체 : DBMS 서버에 접속하여 SQL 명령을 전달하는 기능을 제공하는 객체
-		//DriverManager 클래스 : Driver 객체를 관리하기 위한 기능을 제공하는 클래스
-		//DriverManager.registerDriver(Driver driver) : Driver 객체를 매개변수로 전달받아
-		//DriverManager 클래스가 관리할 수 있는 JDBC Driver 객체로 등록하는 메소드
-		DriverManager.registerDriver(new OracleDriver());
+		try {
+			//1.OracleDriver 클래스로 객체를 생성하여 DriverManager 클래스의 JDBC Driver 객체로 등록
+			// => OracleDriver 클래스를 읽어 메모리에 저장하여 자동으로 OracleDriver 객체를 생성하여
+			//DriverManager 클래스의 JDBC Driver 객체 등록
+			//JDBC Driver 객체 : DriverManager 클래스에 등록되어 관리되는 Driver 객체
+			//Driver 객체 : DBMS 서버에 접속하여 SQL 명령을 전달하는 기능을 제공하는 객체
+			//DriverManager 클래스 : Driver 객체를 관리하기 위한 기능을 제공하는 클래스
+			//DriverManager.registerDriver(Driver driver) : Driver 객체를 매개변수로 전달받아
+			//DriverManager 클래스가 관리할 수 있는 JDBC Driver 객체로 등록하는 메소드
+			// => 동일한 클래스로 생성된 Driver 객체가 DriverManager 클래스에 여러개 등록 가능
+			// => 불필요한 Driver 객체가 존재하여 성능의 저하 발생
+			//DriverManager.registerDriver(new OracleDriver());
+			
+			//Class.forName(String className) 메소드를 호출하여 ClassLoader 프로그램을 이용하여
+			//OracleDriver 클래스를 읽어 메모리에 저장
+			// => OracleDriver 클래스의 정적영역에서 OracleDriver 클래스를 객체로 생성하여
+			//DriverManager 클래스의 JDBC Driver로 등록하는 메소드 호출
+			Class.forName("oracle.jdbc.driver.OracleDriver");
+			
+			//2.DriverManager 클래스에 등록된 JDBC Driver 객체를 이용하여 DBMS 서버에 접속
+			//DriverManager.getConnection(String url, String user, String password)
+			// => DriverManager 클래스에 등록된 JDBC Driver 객체를 이용하여 DBMS 서버에 접속하는 메소드
+			// => DBMS 서버에 접속된 정보가 저장된 Connection 객체를 반환
+			// => 접속 URL 주소의 프로토콜을 이용하여 특정 DBMS 서버에 접속
+			//URL(Uniform Resource Location) : 인터넷에 존재하는 자원의 위치를 표현하는 주소
+			//형식)Protocol:ServerName:Port:Resource >> http://www.itwill.xyz:80/test/index.html
+			//Oracle DBMS 서버에 접속하여 데이타베이스에 접근하기 위한 URL 주소
+			//형식)jdbc:oracle:thin:@ServerName:Port:SID
+			// => 
+			String url="jdbc:oracle:thin:@localhost:1521:xe";
+			String user="scott";
+			String password="tiger";
+			Connection con=DriverManager.getConnection(url, user, password);
+			
+			
+		} catch (ClassNotFoundException e) {
+			System.out.println("OracleDriver 클래스를 찾을 수 없습니다.");
+		} catch (SQLException e) {
+			// TODO: handle exception
+		}
 	}
 }
 
