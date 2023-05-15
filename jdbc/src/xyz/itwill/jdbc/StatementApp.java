9ac157ebc826e30e7385171238e16453cf4450f9
user <user@BOOK-LCCUUB9T5H>
2023-05-15 16:39:10 +0900

    33Day

--- jdbc/src/xyz/itwill/jdbc/StatementApp.java
+++ jdbc/src/xyz/itwill/jdbc/StatementApp.java
@@ -58,7 +58,37 @@ public class StatementApp {
 		ConnectionFactory.close(con, stmt, rs);
 		*/
 
-		//키보드로 
+		//키보드로 이름을 입력받아 STUDENT 테이블에 저장된 학생정보 중 해당 이름의 학생정보를
+		//검색하여 출력하는 JDBC 프로그램 작성
+		
+		//키보드로 학생정보를 입력받기 위한 입력스트림을 생성
+		BufferedReader in=new BufferedReader(new InputStreamReader(System.in));
+		
+		//키보드로 이름을 입력받아 저장
+		System.out.println("<<학생정보 검색>>");
+		System.out.print("이름 입력 >> ");
+		String name=in.readLine();
+		System.out.println("==============================================================");
+		//STUDENT 테이블에 저장된 학생정보 중 해당 이름의 학생정보를 검색하여 출력		
+		Connection con=ConnectionFactory.getConnection();
+		
+		Statement stmt=con.createStatement();
+		
+		String sql="select * from student where name='"+name+"' order by no";
+		ResultSet rs=stmt.executeQuery(sql);
+		
+		System.out.println("<<검색결과>>");
+		if(rs.next()) {
+			do {
+				System.out.println("학번 = "+rs.getInt("no")+", 이름 = "+rs.getString("name")
+					+", 전화번호 = "+rs.getString("phone")+", 주소 = "+rs.getString("address")
+					+", 생년월일 = "+rs.getString("birthday").substring(0, 10));
+			} while(rs.next());
+		} else {
+			System.out.println("검색된 학생정보가 없습니다.");
+		}
+		System.out.println("==============================================================");
+		ConnectionFactory.close(con, stmt, rs);
 	}
 }
 
