1770b1a995857c8348495548023a18ceb782f0c8
user <user@BOOK-LCCUUB9T5H>
2023-05-16 18:03:01 +0900

    34Day

--- jdbc/src/xyz/itwill/student/StudentDAOImpl.java
+++ jdbc/src/xyz/itwill/student/StudentDAOImpl.java
@@ -2,7 +2,9 @@ package xyz.itwill.student;
 
 import java.sql.Connection;
 import java.sql.PreparedStatement;
+import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.util.ArrayList;
 import java.util.List;
 
 //DAO(Data Access Object) 클래스 : 저장매체에 행정보를 삽입,삭제,변경,검색하는 기능을 제공하는 클래스
@@ -55,30 +57,128 @@ public class StudentDAOImpl extends JdbcDAO implements StudentDAO {
 		return rows;
 	}
 
+	//학생정보를 전달받아 STUDENT 테이블에 저장된 학생정보를 변경하고 변경행의 갯수를 반환하는 메소드
 	@Override
 	public int updateStudent(StudentDTO student) {
-		// TODO Auto-generated method stub
-		return 0;
+		Connection con=null;
+		PreparedStatement pstmt=null;
+		int rows=0;
+		try {
+			con=getConnection();
+			
+			String sql="update student set name=?,phone=?,address=?,birthday=? where no=?";
+			pstmt=con.prepareStatement(sql);
+			pstmt.setString(1, student.getName());
+			pstmt.setString(2, student.getPhone());
+			pstmt.setString(3, student.getAddress());
+			pstmt.setString(4, student.getBirthday());
+			pstmt.setInt(5, student.getNo());
+
+			rows=pstmt.executeUpdate();
+		} catch (SQLException e) {
+			System.out.println("[에러]updateStudent() 메소드의 SQL 오류 = "+e.getMessage());
+		} finally {
+			close(con, pstmt);
+		}
+		return rows;
 	}
 
+	//학번을 전달받아 STUDENT 테이블에 저장된 학생정보를 삭제하고 삭제행의 갯수를 반환하는 메소드
 	@Override
 	public int deleteStudent(int no) {
-		// TODO Auto-generated method stub
-		return 0;
+		Connection con=null;
+		PreparedStatement pstmt=null;
+		int rows=0;
+		try {
+			con=getConnection();
+			
+			String sql="delete from student where no=?";
+			pstmt=con.prepareStatement(sql);
+			pstmt.setInt(1, no);
+
+			rows=pstmt.executeUpdate();
+		} catch (SQLException e) {
+			System.out.println("[에러]deleteStudent() 메소드의 SQL 오류 = "+e.getMessage());
+		} finally {
+			close(con, pstmt);
+		}
+		return rows;
 	}
 
+	//학번을 전달받아 STUDENT 테이블에 저장된 해당 학번의 학생정보를 검색하여 반환하는 메소드
 	@Override
 	public StudentDTO selectStudent(int no) {
-		// TODO Auto-generated method stub
-		return null;
+		Connection con=null;
+		PreparedStatement pstmt=null;
+		ResultSet rs=null;
+		StudentDTO student=null;
+		try {
+			con=getConnection();
+			
+			String sql="select * from student where no=?";
+			pstmt=con.prepareStatement(sql);
+			pstmt.setInt(1, no);
+			
+			rs=pstmt.executeQuery();
+			
+			//ResultSet 객체에 저장된 검색행을 Java 객체(값)로 매핑 처리
+			//검색행이 0 또는 1인 경우 선택문 사용
+			if(rs.next()) {//검색행이 있는 경우
+				student=new StudentDTO();
+				//처리행의 컬럼값을 반환받아 DTO 객체의 필드값으로 변경
+				student.setNo(rs.getInt("no"));
+				student.setName(rs.getString("name"));
+				student.setPhone(rs.getString("phone"));
+				student.setAddress(rs.getString("address"));
+				student.setBirthday(rs.getString("birthday").substring(0, 10));
+			}
+		} catch (SQLException e) {
+			System.out.println("[에러]selectStudent() 메소드의 SQL 오류 = "+e.getMessage());
+		} finally {
+			close(con, pstmt, rs);
+		}
+		//검색행이 없는 경우 [null]를 반환하고 검색행이 있으면 DTO 객체 반환
+		return student;
 	}
 
+	//이름을 전달받아 STUDENT 테이블에 저장된 해당 이름의 학생정보를 검색하여 반환하는 메소드
 	@Override
 	public List<StudentDTO> selectNameStudentList(String name) {
-		// TODO Auto-generated method stub
-		return null;
+		Connection con=null;
+		PreparedStatement pstmt=null;
+		ResultSet rs=null;
+		List<StudentDTO> studentList=new ArrayList<>();
+		try {
+			con=getConnection();
+			
+			String sql="select * from student where name=?";
+			pstmt=con.prepareStatement(sql);
+			pstmt.setString(1, name);
+			
+			rs=pstmt.executeQuery();
+			
+			//검색행이 0개 이상인 경우 반복문 사용
+			while(rs.next()) {
+				//하나의 검색행을 DTO 객체로 매핑 처리
+				StudentDTO student=new StudentDTO();
+				student.setNo(rs.getInt("no"));
+				student.setName(rs.getString("name"));
+				student.setPhone(rs.getString("phone"));
+				student.setAddress(rs.getString("address"));
+				student.setBirthday(rs.getString("birthday").substring(0, 10));
+				
+				//DTO 객체를 List 객체의 요소로 추가 
+				studentList.add(student);
+			}
+		} catch (SQLException e) {
+			System.out.println("[에러]selectNameStudentList() 메소드의 SQL 오류 = "+e.getMessage());
+		} finally {
+			close(con, pstmt, rs);
+		}
+		return studentList;
 	}
 
+	//STUDENT 테이블에 저장된 모든 학생정보를 검색하여 반환하는 메소드
 	@Override
 	public List<StudentDTO> selectAllStudentList() {
 		// TODO Auto-generated method stub
