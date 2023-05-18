9aa7ce5d280bc189fcfef5a66491c0776579f8f5
user <user@BOOK-LCCUUB9T5H>
2023-05-18 18:02:14 +0900

    35Day

--- jdbc/src/xyz/itwill/student/WindowBuilderApp.java
+++ jdbc/src/xyz/itwill/student/WindowBuilderApp.java
@@ -3,15 +3,19 @@ package xyz.itwill.student;
 import java.awt.BorderLayout;
 import java.awt.Color;
 import java.awt.EventQueue;
+import java.awt.FlowLayout;
 import java.awt.Font;
 
 import javax.swing.JButton;
+import javax.swing.JDialog;
 import javax.swing.JFrame;
 import javax.swing.JPanel;
 import javax.swing.JScrollPane;
 import javax.swing.JTextField;
 import javax.swing.JTextPane;
 import javax.swing.border.EmptyBorder;
+import java.awt.event.ActionListener;
+import java.awt.event.ActionEvent;
 
 public class WindowBuilderApp extends JFrame {
 	private static final long serialVersionUID = 1L;
@@ -23,6 +27,7 @@ public class WindowBuilderApp extends JFrame {
 	private JTextPane textPane;
 	private JTextField textField;
 
+	Test dialog = new Test(this);
 	/**
 	 * Launch the application.
 	 */
@@ -56,6 +61,11 @@ public class WindowBuilderApp extends JFrame {
 		contentPane.add(panel, BorderLayout.NORTH);
 		
 		red = new JButton("빨간색"); 
+		red.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				dialog.setVisible(true);
+			}
+		});
 		red.setForeground(new Color(255, 0, 0));
 		red.setFont(new Font("굴림체", Font.PLAIN, 20));
 		panel.add(red);
@@ -80,5 +90,39 @@ public class WindowBuilderApp extends JFrame {
 		contentPane.add(textField, BorderLayout.SOUTH);
 		textField.setColumns(10);
 		
+	}
+	
+	public class Test extends JDialog {
+		private static final long serialVersionUID = 1L;
+		private final JPanel contentPanel = new JPanel();
+	
+		public Test(JFrame frame) {
+			super(frame, true);
+			
+			setDefaultCloseOperation(JDialog.HIDE_ON_CLOSE);
+			
+			setBounds(100, 100, 450, 300);
+			getContentPane().setLayout(new BorderLayout());
+			contentPanel.setLayout(new FlowLayout());
+			contentPanel.setBorder(new EmptyBorder(5, 5, 5, 5));
+			getContentPane().add(contentPanel, BorderLayout.CENTER);
+			{
+				JPanel buttonPane = new JPanel();
+				buttonPane.setLayout(new FlowLayout(FlowLayout.RIGHT));
+				getContentPane().add(buttonPane, BorderLayout.SOUTH);
+				{
+					JButton okButton = new JButton("OK");
+					okButton.setActionCommand("OK");
+					buttonPane.add(okButton);
+					getRootPane().setDefaultButton(okButton);
+				}
+				{
+					JButton cancelButton = new JButton("Cancel");
+					cancelButton.setActionCommand("Cancel");
+					buttonPane.add(cancelButton);
+				}
+			}
+		}
+
 	}
 }
