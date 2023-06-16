51e112e06c57b105dda937f913fcfba0eb0b89e4
user <user@BOOK-LCCUUB9T5H>
2023-06-16 12:08:29 +0900

    52Day

--- servlet/src/main/java/xyz/itwill/servlet/UploadServlet.java
+++ servlet/src/main/java/xyz/itwill/servlet/UploadServlet.java
@@ -9,6 +9,20 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+//[multipart/form-data]로 전달된 경우 request 객체로부터 입력스트림(ServletInputStream 객체)을
+//제공받아 임시파일에 저장한 후 전달값과 전달파일을 구분하여 처리
+// => [multipart/form-data]를 처리하는 클래스를 이용하여 처리하는 것을 권장
+
+//[multipart/form-data]를 처리하는 클래스가 포함된 라이브러리를 다운로드 받아 프로젝트에 빌드 처리
+//1.Apache 그룹에서 배포한 commons-fileupload 라이브러리의 클래스 사용 - 선택적 파일 업로드 
+//2.Oreilly 그룹에서 배포한 cos 라이브러리의 클래스 사용 - 무조건 파일 업로드
+
+//Oreilly 그룹에서 배포한 cos 라이브러리를 다운로드 프로젝트 빌드 처리
+//1.http://www.servlets.com 사이트 접속 >> COS File Upload Library 메뉴 클릭 >> cos-22.05.zip 다운로드
+//2.cos-22.05.zip 파일 압축 풀기 >> cos-22.05 폴더 이동 >> lib 폴더 이동 >> cos.jar 복사
+//3.프로젝트 >> src/main/webapp >> WEB-INF >> lib >> cos.jar 붙여넣기
+// => /WEB-INF/lib 폴더에 라이브러리 파일(jar 파일)을 붙여넣기 하면 자동으로 라이브러리 빌드 처리
+
 //입력페이지(fileupload.html)에서 전달된 값과 파일명을 클라이언트에게 전달하여 응답하는 서블릿
 // => 전달파일은 서버 디렉토리에 저장 - 파일 업로드(Upload) 처리
 @WebServlet("/upload.itwill")
@@ -24,12 +38,14 @@ public class UploadServlet extends HttpServlet {
 			return;
 		}
 		
+		/*
 		request.setCharacterEncoding("utf-8");
 		
 		//form 태그의 [multipart/form-data]로 전달된 입력값은 HttpServletRequest 객체의
 		//getParameter() 메소드를 호출하여 반환 불가능
 		String uploader=request.getParameter("uploader");
-		
+		*/
+
 		out.println("<!DOCTYPE html>");
 		out.println("<html>");
 		out.println("<head>");
@@ -39,7 +55,7 @@ public class UploadServlet extends HttpServlet {
 		out.println("<body>");
 		out.println("<h1>파일업로드</h1>");
 		out.println("<hr>");
-		out.println("<p>올린이 = "+uploader+"</p>");
+		//out.println("<p>올린이 = "+uploader+"</p>");
 		out.println("</body>");
 		out.println("</html>");
 	}
