bf5774d9312e80c389bc541e37bdaf14ed2d7ac2
user <user@BOOK-LCCUUB9T5H>
2023-06-16 15:21:14 +0900

    52Day

---
+++ servlet/src/main/java/xyz/itwill/servlet/CookieReadServlet.java
@@ -0,0 +1,22 @@
+package xyz.itwill.servlet;
+
+import java.io.IOException;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+//클라이언트로 전달받은 쿠키값을 반환받아 클라이언트에게 전달하여 응답하는 서블릿 
+@WebServlet("/read.itwill")
+public class CookieReadServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+
+	/**
+	 * @see HttpServlet#service(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void service(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+	}
+
+}
