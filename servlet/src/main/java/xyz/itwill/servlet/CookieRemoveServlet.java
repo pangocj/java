d91dff9c5fd1b9a13f845ad113957b7853c683ff
user <user@BOOK-LCCUUB9T5H>
2023-06-16 16:04:06 +0900

    52Day

---
+++ servlet/src/main/java/xyz/itwill/servlet/CookieRemoveServlet.java
@@ -0,0 +1,22 @@
+package xyz.itwill.servlet;
+
+import java.io.IOException;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+//클라이언트에 저장된 모든 쿠키를 삭제하고 처리결과를 전달하여 응답하는 서블릿
+@WebServlet("/remove.itwill")
+public class CookieRemoveServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+
+	/**
+	 * @see HttpServlet#service(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void service(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+	}
+
+}
