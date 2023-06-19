1f2a9b373cd36ec982220251daee0e710f020f94
user <user@BOOK-LCCUUB9T5H>
2023-06-19 11:31:50 +0900

    52Day

--- servlet/src/main/java/xyz/itwill/servlet/SessionServlet.java
+++ servlet/src/main/java/xyz/itwill/servlet/SessionServlet.java
@@ -13,19 +13,19 @@ import javax.servlet.http.HttpSession;
 
 //세션(Session) : 서버(웹프로그램)와 클라이언트(브라우저)의 연결 지속성을 제공하기 위한 정보를
 //서버에 저장하는 값(객체) - WAS 프로그램에 의해 관리
-// => 보안 관련 정보(권한)를 저장할 목적으로 사용
-// => 세션을 구분하기 위한 식별자(SessionId)를 이용하여 클라이언트를 구별해 세션에 값(객체)를
+// => 보안 관련 정보(권한)를 세션에 저장하여 사용할 목적으로 이용
+// => 세션을 구분하기 위한 식별자(SessionId)를 이용하여 클라이언트를 구분해 세션에 값(객체)를
 //저장하여 사용
-//세션 바인딩(Session Binding) : 연결 지속성을 제공하기 위한 값(객체)이 저장된 세션을 웹프로그램에서
-//사용할 수 있도롤 결합하는 작업 - WAS
+//세션 바인딩(Session Binding) : 연결 지속성을 제공하기 위한 값(객체)이 저장될 세션을 웹프로그램에서
+//사용할 수 있도록 결합하는 작업 - WAS
 
-//클라이언트에게 [JSESSIONID] 이름의 쿠키값을 제공받지 못한 경우 메모리에 새로운 세션을 생성하여
+//클라이언트에게 [JSESSIONID] 이름의 쿠키값을 제공받지 못한 경우 서버 메모리에 새로운 세션을 생성하여
 //바인딩 처리하고 생성된 세션의 식별자(SessionId)를 클라이언트에게 [JSESSIONID] 이름의 쿠키값로 전달
 // => 클라이언트에 저장된 [JSESSIONID] 이름의 쿠키값은 브라우저 종료시 소멸
 //클라이언트에게 [JSESSIONID] 이름의 쿠키값을 제공받은 경우 세션 트랙킹하여 바인딩 처리
 // => 세션 트랙킹이 실패한 경우 새로운 세션을 생성하고 세션의 식별자(SessionId)를 클라이언트에게 전달
 //세션 트랙킹(Session Tracking) : 클라이언트에게 제공받은 [JSESSIONID] 이름의 쿠키값과
-//메모리에 저장된 세션의 식별자(SessionId)를 비교하여 검색하는 작업
+//서버 메모리에 저장된 세션의 식별자(SessionId)를 비교하여 검색하는 작업
 
 //세션을 바인딩하여 클라이언트에게 바인딩된 세션정보를 전달하여 응답하는 서블릿
 @WebServlet("/session.itwill")
@@ -60,7 +60,8 @@ public class SessionServlet extends HttpServlet {
 		//HttpSession.getId() : 세션을 구분하기 위한 식별자(SessionId)를 반환하는 메소드
 		out.println("<p>세션 고유값(SessionId) = "+session.getId()+"</p>");
 		//HttpSession.getMaxInactiveInterval() : 세션 유지시간(초)를 반환하는 메소드 
-		// => 세션 유지시간 : 세션을 사용하지 않을 경우 소멸되도록 설정하는 시간
+		// => 세션 유지시간 : 세션을 사용하지 않을 경우 서버 메모리에서 세션이 자동 소멸되도록 설정하는 시간
+		// => 세션의 기본 유지시간은 30분으로 설정
 		out.println("<p>세션 유지시간 = "+session.getMaxInactiveInterval()+"</p>");
 		
 		//HttpSession.setAttribute(String attributeName, Object attributeValue)
@@ -73,7 +74,7 @@ public class SessionServlet extends HttpServlet {
 
 		//HttpSession.getAttribute(String attributeName) : HttpSession 객체에 저장된 속성값을
 		//반환하는 메소드 - 매개변수에 속성값을 구분하기 위한 속성명 전달
-		// => HttpSession 객체에 저장된 속성값을 Object 객체로 반환하므로 반드시 명시적 객체 형변환 이용
+		// => HttpSession 객체에 저장된 속성값은 Object 객체로 반환되므로 반드시 명시적 객체 형변환 이용
 		// => 매개변수로 전달받은 속성명의 속성값이 없는 경우 [null] 반환
 		Date now=(Date)session.getAttribute("now");
 		out.println("<p>세션에 저장된 속성값(객체) = "+now+"</p>");
