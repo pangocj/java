ee347b6092c743c05c76a5043586ec731b72d85b
user <user@BOOK-LCCUUB9T5H>
2023-06-19 16:01:22 +0900

    53Day

--- servlet/src/main/java/xyz/itwill/dao/StudentDAO.java
+++ servlet/src/main/java/xyz/itwill/dao/StudentDAO.java
@@ -5,7 +5,7 @@ package xyz.itwill.dao;
 // => 싱글톤 클래스(프로그램에 객체를 하나만 생성하여 제공하는 클래스)로 작성하는 것을 권장
 
 //STUDENT 테이블에 학생정보를 삽입,삭제,변경,검색하는 기능을 제공하는 클래스
-public class StudentDAO {
+public class StudentDAO extends JdbcDAO {
 	private static StudentDAO _dao;
 	
 	private StudentDAO() {
