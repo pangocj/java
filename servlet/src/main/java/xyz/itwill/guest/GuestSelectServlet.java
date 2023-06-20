af60516534017693fb91de24f1c801b3c1dc5c32
user <user@BOOK-LCCUUB9T5H>
2023-06-20 11:32:22 +0900

    54Day

--- servlet/src/main/java/xyz/itwill/guest/GuestSelectServlet.java
+++ servlet/src/main/java/xyz/itwill/guest/GuestSelectServlet.java
@@ -69,9 +69,12 @@ public class GuestSelectServlet extends HttpServlet {
 				out.println("<tr>");
 				out.println("<th width='150'>내용</th>");
 				out.println("<td width='650' colspan='3'>"+guest.getContent().replace("\n", "<br>")+"</td>");
-				out.println("<br>");
+				out.println("</tr>");
+				out.println("<tr>");
+				out.println("<td align='right' colspan='4'>");
 				out.println("<button type='button'>변경</button>");
 				out.println("<button type='button'>삭제</button>");
+				out.println("</td");
 				out.println("</tr>");
 				out.println("</table>");
 				out.println("</td>");
