d68012dd718299d350d51affe3447db0b4fe8608
user <user@BOOK-LCCUUB9T5H>
2023-06-27 13:16:45 +0900

    58Day

--- jsp/src/main/java/xyz/itwill/dao/MemberDAO.java
+++ jsp/src/main/java/xyz/itwill/dao/MemberDAO.java
@@ -2,6 +2,7 @@ package xyz.itwill.dao;
 
 import java.sql.Connection;
 import java.sql.PreparedStatement;
+import java.sql.ResultSet;
 import java.sql.SQLException;
 
 import xyz.itwill.dto.MemberDTO;
@@ -47,6 +48,43 @@ public class MemberDAO extends JdbcDAO {
 			close(con, pstmt);
 		}
 		return rows;
+	}
+	
+	//아이디를 전달받아 MEMBER 테이블에 저장된 회원정보를 검색하여 DTO 객체로 반환하는 메소드
+	public MemberDTO selectMember(String id) {
+		Connection con=null;
+		PreparedStatement pstmt=null;
+		ResultSet rs=null;
+		MemberDTO member=null;
+		try {
+			con=getConnection();
+			
+			String sql="select * from member where id=?";
+			pstmt=con.prepareStatement(sql);
+			pstmt.setString(1, id);
+			
+			rs=pstmt.executeQuery();
+			
+			if(rs.next()) {
+				member=new MemberDTO();
+				member.setId(rs.getString("id"));
+				member.setPasswd(rs.getString("passwd"));
+				member.setName(rs.getString("name"));
+				member.setEmail(rs.getString("email"));
+				member.setMobile(rs.getString("mobile"));
+				member.setZipcode(rs.getString("zipcode"));
+				member.setAddress1(rs.getString("address1"));
+				member.setAddress2(rs.getString("address2"));
+				member.setJoinDate(rs.getString("join_date"));
+				member.setLastLogin(rs.getString("last_login"));
+				member.setMemberStatus(rs.getInt("member_status"));
+			}
+ 		} catch (SQLException e) {
+			System.out.println("[에러]selectMember() 메소드의 SQL 오류 = "+e.getMessage());
+		} finally {
+			close(con, pstmt, rs);
+		}
+		return member;
 	}
 }
 
--- jsp/src/main/webapp/member/id_check.jsp
+++ jsp/src/main/webapp/member/id_check.jsp
@@ -1,2 +1,36 @@
 <%@ page language="java" contentType="text/html; charset=UTF-8"
     pageEncoding="UTF-8"%>
+<%-- 아이디를 전달받아 MEMBER 테이블의 저장된 회원정보의 아이디와 비교하여 중복 결과값을 전달하여 응답하는 JSP 문서 --%>
+<%-- => 아이디 미중복 : 아이디 사용 가능 메세지 출력 --%>
+<%-- => 아이디 중복 : 아이디 사용 불가능 메세지 출력 --%>
+<%
+	//전달값이 없는 경우 - 비정상적인 요청
+	if(request.getParameter("id")==null) {
+		response.sendError(HttpServletResponse.SC_BAD_REQUEST);
+		return;
+	}
+
+	//전달값을 반환받아 저장
+	String id=request.getParameter("id");
+	
+	//아이디를 전달받아 MEMBER 테이블에 저장된 회원정보를 검색하여 DTO 객체로 반환하는 DAO 클래스의 메소드 호출
+	
+%>
+<!DOCTYPE html>
+<html>
+<head>
+<meta charset="UTF-8">
+<title>JSP</title>
+</head>
+<body>
+
+</body>
+</html>
+
+
+
+
+
+
+
+
