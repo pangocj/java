b2186e61a8dae41ca68bac713573d9e728d7debe
user <user@BOOK-LCCUUB9T5H>
2023-06-29 11:13:02 +0900

    60Day

---
+++ jsp/src/main/java/xyz/itwill/dto/ReviewDTO.java
@@ -0,0 +1,26 @@
+package xyz.itwill.dto;
+
+//create table review(num number primary key, id varchar2(30), subject varchar2(500)
+//    , content varchar2(4000), regdate date, readcount number, ref number(4)
+//	  , restep number(4), relevel number(4), ip varchar2(20), status number(1));
+//create sequence review_seq; 
+
+/* 
+이름        널?       유형             
+--------- -------- -------------- 
+NUM       NOT NULL NUMBER         
+ID                 VARCHAR2(30)   
+SUBJECT            VARCHAR2(500)  
+CONTENT            VARCHAR2(4000) 
+REGDATE            DATE           
+READCOUNT          NUMBER         
+REF                NUMBER(4)      
+RESTEP             NUMBER(4)      
+RELEVEL            NUMBER(4)      
+IP                 VARCHAR2(20)   
+STATUS             NUMBER(1)       
+*/
+
+public class ReviewDTO {
+
+}
