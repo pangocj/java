f0737e1e159be97ded1c5b711ab65bd69ecb4c60
user <user@BOOK-LCCUUB9T5H>
2023-07-11 12:41:50 +0900

    67Day

--- mvc/src/main/java/xyz/itwill/mvc/WriteModel.java
+++ mvc/src/main/java/xyz/itwill/mvc/WriteModel.java
@@ -47,7 +47,7 @@ public class WriteModel implements Action {
 			
 			actionForward.setForward(false);
 			actionForward.setPath(request.getContextPath()+"/loginform.do");
-		}catch (ExistsUserinfoException e) {
+		} catch (ExistsUserinfoException e) {
 			//아이디가 중복된 경우 발생되는 예외에 대한 예외처리 명령 작성
 			request.setAttribute("message", e.getMessage());
 			request.setAttribute("userinfo", userinfo);
