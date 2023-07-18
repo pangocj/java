42d4825c8464740883af8bbb129c20b8b8cc2220
user <user@BOOK-LCCUUB9T5H>
2023-07-18 15:20:02 +0900

    73Day

---
+++ mybatis/src/main/java/xyz/itwill/dto/MyComment1.java
@@ -0,0 +1,20 @@
+package xyz.itwill.dto;
+
+/*
+create table mycomment(comment_no number primary key, comment_id varchar2(50)
+    , comment_content varchar2(100), comment_date date);
+    
+create sequence mycomment_seq;  
+
+
+이름              널?       유형            
+--------------- -------- ------------- 
+COMMENT_NO      NOT NULL NUMBER        
+COMMENT_ID               VARCHAR2(50)  
+COMMENT_CONTENT          VARCHAR2(100) 
+COMMENT_DATE             DATE   
+*/
+
+public class MyComment1 {
+
+}
