102d05b57aaf410969733bc3b6b913352d4cded3
user <user@BOOK-LCCUUB9T5H>
2023-07-19 14:46:49 +0900

    74Day

---
+++ mybatis/src/main/java/xyz/itwill/dto/MyReply.java
@@ -0,0 +1,21 @@
+package xyz.itwill.dto;
+
+
+/*
+create table myreply(reply_no number primary key, reply_id varchar2(50)
+    , reply_content varchar2(100), reply_date date, reply_comment_no number);
+    
+create sequence myreply_seq;   
+
+이름               널?       유형            
+---------------- -------- ------------- 
+REPLY_NO         NOT NULL NUMBER        
+REPLY_ID                  VARCHAR2(50)  
+REPLY_CONTENT             VARCHAR2(100) 
+REPLY_DATE                DATE          
+REPLY_COMMENT_NO          NUMBER  
+*/
+
+public class MyReply {
+
+}
