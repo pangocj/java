777612881b547f10b0ce3a40e1681822eaf86867
user <user@BOOK-LCCUUB9T5H>
2023-07-19 16:29:39 +0900

    74Day

--- mybatis/src/main/java/xyz/itwill/mapper/MyReplyMapper.java
+++ mybatis/src/main/java/xyz/itwill/mapper/MyReplyMapper.java
@@ -3,9 +3,11 @@ package xyz.itwill.mapper;
 import java.util.List;
 
 import xyz.itwill.dto.MyReply;
+import xyz.itwill.dto.MyReplyUser;
 
 public interface MyReplyMapper {
 	int insertReply(MyReply reply);
 	List<MyReply> selectReplyList();
 	List<MyReply> selectCountReplyList();
+	List<MyReplyUser> selectReplyUserList1();
 }
