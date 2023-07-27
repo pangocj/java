9d30b48e6a54f0db20334b26008a16dbf90d2585
user <user@BOOK-LCCUUB9T5H>
2023-07-27 13:18:45 +0900

    80Day

--- spring/src/main/java/xyz/itwill05/di/CollectionBean.java
+++ spring/src/main/java/xyz/itwill05/di/CollectionBean.java
@@ -6,6 +6,10 @@ import java.util.Set;
 public class CollectionBean {
 	private Set<String> nameSet;
 	private List<String> nameList;
+	//콜렉션 객체의 제네릭을 인터페이스로 설정하면 콜렉션 객체에는 인터페이스를 상속받은
+	//모든 자식클래스로 생성된 객체를 요소 추가 가능
+	private Set<Controller> controllerSet;
+	private List<Controller> controllerList;
 	
 	public CollectionBean() {
 		System.out.println("### CollectionBean 클래스의 기본 생성자 호출 ###");
@@ -25,6 +29,22 @@ public class CollectionBean {
 
 	public void setNameList(List<String> nameList) {
 		this.nameList = nameList;
+	}
+
+	public Set<Controller> getControllerSet() {
+		return controllerSet;
+	}
+
+	public void setControllerSet(Set<Controller> controllerSet) {
+		this.controllerSet = controllerSet;
+	}
+
+	public List<Controller> getControllerList() {
+		return controllerList;
+	}
+
+	public void setControllerList(List<Controller> controllerList) {
+		this.controllerList = controllerList;
 	}
 	
 	
