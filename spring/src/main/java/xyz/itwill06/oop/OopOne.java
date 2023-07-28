7d366ca4172febb668c6401898a9fd3dcba9bdc1
user <user@BOOK-LCCUUB9T5H>
2023-07-28 15:54:43 +0900

    81Day

---
+++ spring/src/main/java/xyz/itwill06/oop/OopOne.java
@@ -0,0 +1,15 @@
+package xyz.itwill06.oop;
+
+public class OopOne {
+	public void display1() {
+		System.out.println("*** OopOne 클래스의 display1() 메소드 호출 ***");
+	}
+	
+	public void display2() {
+		System.out.println("*** OopOne 클래스의 display2() 메소드 호출 ***");
+	}
+	
+	public void display3() {
+		System.out.println("*** OopOne 클래스의 display3() 메소드 호출 ***");
+	}
+}
---
+++ spring/src/main/java/xyz/itwill06/oop/OopTwo.java
@@ -0,0 +1,15 @@
+package xyz.itwill06.oop;
+
+public class OopTwo {
+	public void display1() {
+		System.out.println("*** OopTwo 클래스의 display1() 메소드 호출 ***");
+	}
+	
+	public void display2() {
+		System.out.println("*** OopTwo 클래스의 display2() 메소드 호출 ***");
+	}
+	
+	public void display3() {
+		System.out.println("*** OopTwo 클래스의 display3() 메소드 호출 ***");
+	}
+}
