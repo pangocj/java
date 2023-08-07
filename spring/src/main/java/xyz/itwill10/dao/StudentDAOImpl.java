fe9e95085d3bdb492f8ee2501a6c78ea819d2e6a
user <user@BOOK-LCCUUB9T5H>
2023-08-07 17:00:47 +0900

    87Day

---
+++ spring/src/main/java/xyz/itwill10/dao/StudentDAOImpl.java
@@ -0,0 +1,10 @@
+package xyz.itwill10.dao;
+
+//SpringMVC 기능으로 웹프로그램 작성시 Mybatis 프레임워크를 이용하여 DAO 클래스를 작성하는 방법
+//1.DataSource 관련 라이브러리와 Mybatis 관련 라이브러리를 프로젝트에 빌드 처리 - 메이븐 : pom.xml
+// => ojdbc, spring-jdbc(spring-tx), mybatis, mybatis-spring
+
+
+public class StudentDAOImpl {
+
+}
