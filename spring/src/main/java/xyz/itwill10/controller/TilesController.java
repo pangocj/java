df08448970df5df47f7f1b1fa5967644dbc3ec53
user <user@BOOK-LCCUUB9T5H>
2023-08-09 17:03:08 +0900

    89Day

--- spring/src/main/java/xyz/itwill10/controller/TilesController.java
+++ spring/src/main/java/xyz/itwill10/controller/TilesController.java
@@ -3,6 +3,8 @@ package xyz.itwill10.controller;
 //TilesView 클래스 : 요청 처리 메소드의 반환값(ViewName)을 제공받아 다수의 JSP 문서가 결합된
 //JSP 문서(템플릿 페이지)로 응답하기 위한 기능을 제공하는 클래스 - ViewResolver
 //1.TilesView 관련 라이브러리(tiles-extras 라이브러리)를 프로젝트에 빌드 처리 - 메이븐 : pom.xml
+//2.요청 처리 메소드의 반환값(ViewName)을 제공받아 응답할 템플릿 페이지 설정
+// => TilesView 프로그램의 환경설정파일을 설정 - /WEB-INF/spring/appServlet/tiles.xml
 
 public class TilesController {
 
