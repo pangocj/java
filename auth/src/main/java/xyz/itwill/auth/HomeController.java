16306ce9e9088bd4d6aea246b81b5c99725c32ec
user <user@BOOK-LCCUUB9T5H>
2023-09-05 11:43:22 +0900

    106Day

--- auth/src/main/java/xyz/itwill/auth/HomeController.java
+++ auth/src/main/java/xyz/itwill/auth/HomeController.java
@@ -1,39 +1,28 @@
 package xyz.itwill.auth;
 
-import java.text.DateFormat;
-import java.util.Date;
-import java.util.Locale;
-
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
-/**
- * Handles requests for the application home page.
- */
 @Controller
 public class HomeController {
-	
-	private static final Logger logger = LoggerFactory.getLogger(HomeController.class);
-	
-	/**
-	 * Simply selects the home view to render by returning its name.
-	 */
 	@RequestMapping(value = "/", method = RequestMethod.GET)
-	public String home(Locale locale, Model model) {
-		logger.info("Welcome home! The client locale is {}.", locale);
-		
-		Date date = new Date();
-		DateFormat dateFormat = DateFormat.getDateTimeInstance(DateFormat.LONG, DateFormat.LONG, locale);
-		
-		String formattedDate = dateFormat.format(date);
-		
-		model.addAttribute("serverTime", formattedDate );
-		
+	public String home() {
 		return "home";
 	}
 	
+	@RequestMapping(value = "/user/page", method = RequestMethod.GET)
+	public String userPage() {
+		return "user_page";
+	}
+	
+	@RequestMapping(value = "/manager/page", method = RequestMethod.GET)
+	public String managerPage() {
+		return "manager_page";
+	}
+	
+	@RequestMapping(value = "/admin/page", method = RequestMethod.GET)
+	public String adminPage() {
+		return "admin_page";
+	}
 }
