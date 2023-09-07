2fb9f4cd3a29a426c57c1ac24e5083ad48573743
user <user@BOOK-LCCUUB9T5H>
2023-09-07 11:35:35 +0900

    108Day

---
+++ auth/src/main/java/xyz/itwill/repository/SecurityUsersRepository.java
@@ -0,0 +1,9 @@
+package xyz.itwill.repository;
+
+import xyz.itwill.dto.SecurityAuth;
+import xyz.itwill.dto.SecurityUsers;
+
+public interface SecurityUsersRepository {
+	int insertSecurityUsers(SecurityUsers users);
+	int insertSecurityAuth(SecurityAuth auth);
+}
