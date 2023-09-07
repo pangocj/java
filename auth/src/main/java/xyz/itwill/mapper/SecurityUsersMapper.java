54abb0444ba508db4031288131b647b629e3d3d1
user <user@BOOK-LCCUUB9T5H>
2023-09-07 11:31:37 +0900

    1098Day

---
+++ auth/src/main/java/xyz/itwill/mapper/SecurityUsersMapper.java
@@ -0,0 +1,9 @@
+package xyz.itwill.mapper;
+
+import xyz.itwill.dto.SecurityAuth;
+import xyz.itwill.dto.SecurityUsers;
+
+public interface SecurityUsersMapper {
+	int insertSecurityUsers(SecurityUsers users);
+	int insertSecurityAuth(SecurityAuth auth);
+}
